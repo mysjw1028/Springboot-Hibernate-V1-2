0f2ebce6cb6b35e81c80769cfad926ad4b65d781
mysjw1028 <108706248+mysjw1028@users.noreply.github.com>
2022-10-29 20:37:44 +0900

    강사님 깃 맞춤

--- src/test/java/site/metacoding/white/web/CommentApiControllerTest.java
+++ src/test/java/site/metacoding/white/web/CommentApiControllerTest.java
@@ -24,20 +24,19 @@ import site.metacoding.white.domain.Comment;
 import site.metacoding.white.domain.CommentRepository;
 import site.metacoding.white.domain.User;
 import site.metacoding.white.domain.UserRepository;
+import site.metacoding.white.dto.CommentReqDto.CommentSaveReqDto;
 import site.metacoding.white.dto.SessionUser;
-import site.metacoding.white.dto.UserReqDto.JoinReqDto;
-import site.metacoding.white.dto.UserReqDto.LoginReqDto;
 import site.metacoding.white.util.SHA256;
 
 @ActiveProfiles("test")
 @Sql("classpath:truncate.sql")
-@Transactional // 트랜잭션 안붙이면 영속성 컨텍스트에서 DB로 flush 안됨 (Hibernate 사용시) // @Transactional //
-               // 통합테스트에서 RANDOM_PORT를 사용하면 새로운 스레드로 돌기 때문에 rollback 무의미
+@Transactional // 트랜잭션 안붙이면 영속성 컨텍스트에서 DB로 flush 안됨 (Hibernate 사용시)
 @AutoConfigureMockMvc // MockMvc Ioc 컨테이너에 등록
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK) // 가짜 환경으로 실행
-public class UserApiControllerTest {
+public class CommentApiControllerTest {
 
     private static final String APPLICATION_JSON = "application/json; charset=utf-8";
+
     @Autowired
     private MockMvc mvc;
 
@@ -57,9 +56,6 @@ public class UserApiControllerTest {
     private SHA256 sha256;
 
     private MockHttpSession session;
-
-    // @Autowired
-    // private UserService userService; // 이렇게해야 controller 안때림
 
     @BeforeEach
     public void sessionInit() {
@@ -97,44 +93,37 @@ public class UserApiControllerTest {
         commentRepository.save(comment2);
     }
 
-    // @Order(1)
     @Test
-    public void join_test() throws Exception {
+    public void save_test() throws Exception {
         // given
-        JoinReqDto joinReqDto = new JoinReqDto();
-        joinReqDto.setUsername("cos");
-        joinReqDto.setPassword("1234");
+        CommentSaveReqDto commentSaveReqDto = new CommentSaveReqDto();
+        commentSaveReqDto.setContent("추천해요");
+        commentSaveReqDto.setBoardId(1L);
 
-        String body = om.writeValueAsString(joinReqDto);
+        String body = om.writeValueAsString(commentSaveReqDto);
 
         // when
         ResultActions resultActions = mvc
-                .perform(MockMvcRequestBuilders.post("/join").content(body)
-                        .contentType(APPLICATION_JSON).accept(APPLICATION_JSON));
+                .perform(MockMvcRequestBuilders.post("/comment").content(body)
+                        .contentType(APPLICATION_JSON).accept(APPLICATION_JSON)
+                        .session(session));
 
         // then
         MvcResult mvcResult = resultActions.andReturn();
         System.out.println("디버그 : " + mvcResult.getResponse().getContentAsString());
         resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1L));
     }
-    // then
-    // System.out.println(response.getStatusCode());
-    // System.out.println(response.getBody());
 
     @Test
-    public void login_test() throws Exception {
+    public void delete_test() throws Exception {
         // given
-        LoginReqDto loginReqDto = new LoginReqDto();
-        loginReqDto.setUsername("ssar"); // ssar 로 해야함.
-        loginReqDto.setPassword("1234");
-        String body = om.writeValueAsString(loginReqDto);
-
-        // 위에서 검증해서 잘 됐으니 얘는 검증할 필요가 없다 무조건 save가 됨!!
+        Long id = 1L;
 
         // when
         ResultActions resultActions = mvc
-                .perform(MockMvcRequestBuilders.post("/login").content(body)
-                        .contentType(APPLICATION_JSON).accept(APPLICATION_JSON));
+                .perform(MockMvcRequestBuilders.delete("/comment/" + id)
+                        .accept(APPLICATION_JSON)
+                        .session(session));
 
         // then
         MvcResult mvcResult = resultActions.andReturn();
@@ -142,4 +131,3 @@ public class UserApiControllerTest {
         resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1L));
     }
 }
-// inset 로직하고 login
