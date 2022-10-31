569822235a44b326e17432fa41ed0bd1f300b3ab
mysjw1028 <mysjw1028@naver.com>
2022-10-31 15:16:15 +0900

    JWT 토큰생성 완료

--- src/main/java/site/metacoding/white/config/FilterConfig.java
+++ src/main/java/site/metacoding/white/config/FilterConfig.java
@@ -1,52 +1,32 @@
 package site.metacoding.white.config;
 
-import java.io.IOException;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import site.metacoding.white.config.auth.JwtAuthenticationFilter;
+import site.metacoding.white.domain.UserRepository;
 
 @Slf4j
 @Configuration // IOC에 등록이 됨
-public class FilterConfig {
+@RequiredArgsConstructor
+public class FilterConfig { // di실제로는 여기서함
+
+    private final UserRepository userRepository;// DI(스프링IOC컨터이너에서 옴)
 
     @Bean // IOC에등록이됨(서버실행시)
-    public FilterRegistrationBean<HelloFilter> jwtAuthenticationFilterRegister() {
+    public FilterRegistrationBean<JwtAuthenticationFilter> jwtAuthenticationFilterRegister() {
         log.debug("디버그 : 인증 필터 등록");
-        FilterRegistrationBean<HelloFilter> bean = new FilterRegistrationBean<>(new HelloFilter());
-        bean.addUrlPatterns("/hello");
+        FilterRegistrationBean<JwtAuthenticationFilter> bean = new FilterRegistrationBean<>(
+                new JwtAuthenticationFilter(userRepository));// 실제로 DI되서 여기 들어옴
+        bean.addUrlPatterns("/login");
         return bean;
+        // jwt토큰을 만들어줘야함! /아이디.유저네임.패스워드
+        // / login일때 아이디.유저네임.패스워드 확인
+        // 토큰을 만들어줌
     }
+
+    // 추가적인 필터 등록시
 }
-
-@Slf4j
-class HelloFilter implements Filter {// 스프링에들오기전에 실행
-    // /hello 요청시
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        HttpServletRequest req = (HttpServletRequest) request;
-        HttpServletResponse resp = (HttpServletResponse) response;
-
-        if (req.getMethod().equals("POST")) {
-            log.debug("디버그 : HelloFilter 실행됨");
-        } else {
-            log.debug("디버그 : HelloFilter 실행됨");
-        }
-
-        // chain.doFilter(request, response);
-    }// chain.doFilter(request, response);를 안걸면 통신이 종료되서 하얀화면만 나옴
-     // -> 전달을 해야함!!ㄴ
-
-}
-// 필터는chain이 되어있다.
--- src/main/java/site/metacoding/white/domain/UserJpaRepository.java
+++ src/main/java/site/metacoding/white/domain/UserJpaRepository.java
@@ -1,7 +1,6 @@
 package site.metacoding.white.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface UserJpaRepository extends JpaRepository<User, Long> {
